ac3ab75b406cb2fe6154978e8b1a8c8c567f416f
Momina Babar <momina.babar@auxcube.com>
2024-07-26 14:05:21 +0500

    try

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => (
   <Button
     onClick={() => alert("Now everything's alright! If not, check reality settings.")}
   >
-    Make everything alright
+   yooooooooo
   </Button>
 )
 
